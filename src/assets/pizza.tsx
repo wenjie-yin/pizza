e02221bfb4135dfdb50a106de2c8db4f724dff61
w.yin <w.yin@qmul.ac.uk>
2025-03-18 12:55:33 +0000

    conditional margin

--- src/assets/pizza.tsx
+++ src/assets/pizza.tsx
@@ -19,10 +19,11 @@ export function Pizza( { pizza, toppings } : PizzaProps ) {
     });
 
     const justifyDirection = pizza.direction === 'bl' || pizza.direction === 'br' ? 'justify-start' : 'justify-end' ;
+    const marginLeft = pizza.direction === 'tl' || pizza.direction === 'bl' ? 'ml-[10px]' : '';
 
     return (
     <div ref={setNodeRef} className={`quadrant ${pizza.direction} flex flex-col ${justifyDirection}`}>
-      <div className='grid grid-cols-5'>
+      <div className={`grid grid-cols-5 w-50 ${marginLeft}`}>
         {toppings.map(topping => (
           <Topping topping={topping} /> ))}
       </div>
